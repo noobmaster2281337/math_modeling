1533821331a2f34b151eba0a780479bf5da25a5a
noobmaster22813 <93728512+noobmaster2281337@users.noreply.github.com>
2021-12-01 12:35:12 +0000

    w

--- lec_main_33.py
+++ lec_main_33.py
@@ -1,6 +1,14 @@
-import numpy as np
-v0x = float(input('v0x = '))
-x0 = float(input('x0 = '))
-y0 = float(input('y0 = '))
-
 from module_of_physical_constants import g
+import numpy as np
+x0 = 2
+y0 = 3
+V0 = 4
+t = np.linspace(0,5,100)
+x = x0 + V0*t
+y = y0 + V0*t - (g*(t**2))/2
+A = np.zeros((100,3))
+for i in range(100):
+  A[i,0]=t[i]
+  A[i,1]=x[i]
+  A[i,2]=y[i]
+print(A)
